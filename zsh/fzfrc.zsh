758e3efa4cba8465c2cf8ef16e88973c90a8722f
Mohammed Alrefai <5350216+Alrefai@users.noreply.github.com>
2018-11-11 21:59:10 +0300

    Reconfigure fzf

--- zsh/fzfrc.zsh
+++ zsh/fzfrc.zsh
@@ -4,7 +4,7 @@ source ~/.fzf.zsh
 export FZF_TMUX=1
 
 # Use `tree` command to show the entries of the directory
-export FZF_ALT_C_OPTS="--preview 'tree -C {} | head -200'"
+export FZF_ALT_C_OPTS="--preview 'tree -C {} -I node_modules | head -200'"
 
 # Run fzf in full screen mode with more options
 export FZF_DEFAULT_OPTS='--bind J:down,K:up --reverse --ansi --multi --no-height --no-reverse --select-1 --exit-0'
@@ -13,14 +13,14 @@ export FZF_DEFAULT_OPTS='--bind J:down,K:up --reverse --ansi --multi --no-height
 export FZF_CTRL_R_OPTS="--preview 'echo {}' --preview-window down:3:hidden:wrap --bind '?:toggle-preview'"
 
 # Use 'bfs` to browse folders
-export FZF_ALT_C_COMMAND="bfs -type d"
+export FZF_ALT_C_COMMAND="bfs -type d -nohidden -not -path '*/node_modules/*'"
 
 # Preview the content of the file under the cursor
 # Using highlight (http://www.andre-simon.de/doku/highlight/en/highlight.html)
 export FZF_CTRL_T_OPTS="--preview '(highlight -O ansi -l {} 2> /dev/null || cat {} || tree -C {}) 2> /dev/null | head -200'"
 
 # ---- bag-man/dotfiles/bashrc ------------------------------------------------
-export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules,*.swp,dist,*.coffee}/*" 2> /dev/null'
+export FZF_DEFAULT_COMMAND='rg --files --no-ignore --hidden --follow -g "!{.git,node_modules,*.swp,dist,*.coffee,dotbot,.next}/*" 2> /dev/null'
 
 export FZF_CTRL_T_COMMAND="$FZF_DEFAULT_COMMAND"
 
