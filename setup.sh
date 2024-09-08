da5c0353442960995ba811327127ffe077a74a11
Mohammed Alrefai <mohammed@refam.io>
2024-09-08 22:25:26 +0300

    feat: new setup script

--- setup.sh
+++ setup.sh
@@ -1,19 +1,32 @@
 #!/usr/bin/env bash
 
-# install nix via determinate nix installer
 if ! command -v nix >/dev/null; then
-  sh -s -- install --extra-conf "trusted-users = $(whoami)" < <(
-    curl --proto '=https' --tlsv1.2 -sSf -L \
-      https://install.determinate.systems/nix
-  ) || exit 1
+  echo 'Installing nix via lix installer...'
+  curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix |
+    sh -s -- install \
+    --extra-conf 'auto-optimise-store = true' \
+    --extra-conf 'use-xdg-base-directories = true' \
+    --extra-conf 'trusted-users = root @adm @admin @wheel' || exit 1
 
-  # run nix daemon
+  echo
   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
+  echo 'Starting nix daemon...'
 else
-  mkdir -p ~/.config/nix
-  echo 'experimental-features = nix-command flakes' >> ~/.config/nix/nix.conf
+  if [[ -e /etc/NIXOS ]]; then
+    echo 'This system is running NixOS.'
+    mkdir -p ~/.config/nix
+    echo 'experimental-features = nix-command flakes' >> ~/.config/nix/nix.conf
+    echo 'use-xdg-base-directories = true' >> ~/.config/nix/nix.conf
+    echo 'Added extra configurations to ~/.config/nix/nix.conf'
+    echo
+    echo "Rebuild NixOS configurations from dotfiles repo..."
+    sudo nixos-rebuild switch --flake github:alrefai/dotfiles/switch-to-lix#nixos --impure
+  else
+    echo 'This system is not running NixOS.'
+  fi
 fi
 
-# start a shell with git and neovim in the environment
-nix shell nixpkgs/nixos-24.05#{git,neovim}
+echo
+echo "Switching home-manager configurations from dotfiles repo..."
+nix run home-manager/master -- switch -b bak --flake github:alrefai/dotfiles/switch-to-lix
 
