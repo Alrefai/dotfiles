7d85c031de14363776312d176679a1a8c434ce91
Mohammed Alrefai <mohammed@refam.io>
2024-09-11 22:09:52 +0300

    feat: add shell script formatters with treefmt

--- treefmt.nix
+++ treefmt.nix
@@ -3,8 +3,11 @@ _: {
   programs = {
     alejandra.enable = true;
     statix.enable = true;
+    shellcheck.enable = true;
+    shfmt.enable = true;
   };
   settings = {
     global.excludes = ["bin/*" ".editorconfig" "*/ssh/*"];
+    formatter.shfmt.priority = 1;
   };
 }
