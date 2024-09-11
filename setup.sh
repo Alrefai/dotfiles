78aab87f19d7c22ae6f3e11bfa96bd062e4e3cd1
Mohammed Alrefai <mohammed@refam.io>
2024-09-11 22:11:32 +0300

    style: format setup script

--- setup.sh
+++ setup.sh
@@ -4,19 +4,20 @@ if ! command -v nix >/dev/null; then
   echo 'Installing nix via lix installer...'
   curl --proto '=https' --tlsv1.2 -sSf -L https://install.lix.systems/lix |
     sh -s -- install \
-    --extra-conf 'auto-optimise-store = true' \
-    --extra-conf 'use-xdg-base-directories = true' \
-    --extra-conf 'trusted-users = root @adm @admin @wheel' || exit 1
+      --extra-conf 'auto-optimise-store = true' \
+      --extra-conf 'use-xdg-base-directories = true' \
+      --extra-conf 'trusted-users = root @adm @admin @wheel' || exit 1
 
   echo
+  # shellcheck source=/dev/null
   . /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh
   echo 'Starting nix daemon...'
 else
   if [[ -e /etc/NIXOS ]]; then
     echo 'This system is running NixOS.'
     mkdir -p ~/.config/nix
-    echo 'experimental-features = nix-command flakes' >> ~/.config/nix/nix.conf
-    echo 'use-xdg-base-directories = true' >> ~/.config/nix/nix.conf
+    echo 'experimental-features = nix-command flakes' >>~/.config/nix/nix.conf
+    echo 'use-xdg-base-directories = true' >>~/.config/nix/nix.conf
     echo 'Added extra configurations to ~/.config/nix/nix.conf'
     echo
     echo "Rebuild NixOS configurations from dotfiles repo..."
@@ -29,4 +30,3 @@ fi
 echo
 echo "Switching home-manager configurations from dotfiles repo..."
 nix run home-manager/master -- switch -b bak --flake github:alrefai/dotfiles
-
