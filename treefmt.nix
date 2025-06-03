e66080145518587bbaedc167aa671361fe372248
Mohammed Alrefai <mohammed@refam.io>
2025-06-03 17:07:15 +0300

    chore(fmt): update dprint plugins

--- treefmt.nix
+++ treefmt.nix
@@ -32,16 +32,16 @@ _: {
         excludes = ["node_modules" "*-lock.json" "*-lock.yaml" "dist"];
         # https://plugins.dprint.dev
         plugins = [
-          "https://plugins.dprint.dev/typescript-0.93.0.wasm"
-          "https://plugins.dprint.dev/json-0.19.3.wasm"
-          "https://plugins.dprint.dev/markdown-0.17.8.wasm"
-          "https://plugins.dprint.dev/toml-0.6.2.wasm"
-          "https://plugins.dprint.dev/dockerfile-0.3.2.wasm"
+          "https://plugins.dprint.dev/typescript-0.95.5.wasm"
+          "https://plugins.dprint.dev/json-0.20.0.wasm"
+          "https://plugins.dprint.dev/markdown-0.18.0.wasm"
+          "https://plugins.dprint.dev/toml-0.7.0.wasm"
+          "https://plugins.dprint.dev/dockerfile-0.3.3.wasm"
           "https://plugins.dprint.dev/ruff-0.3.9.wasm"
-          "https://plugins.dprint.dev/jupyter-0.1.4.wasm"
-          "https://plugins.dprint.dev/g-plane/markup_fmt-v0.12.0.wasm"
-          "https://plugins.dprint.dev/g-plane/pretty_yaml-v0.5.0.wasm"
-          "https://plugins.dprint.dev/g-plane/pretty_graphql-v0.2.0.wasm"
+          "https://plugins.dprint.dev/jupyter-0.2.0.wasm"
+          "https://plugins.dprint.dev/g-plane/markup_fmt-v0.20.0.wasm"
+          "https://plugins.dprint.dev/g-plane/pretty_yaml-v0.5.1.wasm"
+          "https://plugins.dprint.dev/g-plane/pretty_graphql-v0.2.1.wasm"
         ];
       };
     };
