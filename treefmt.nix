c88ed011fb3d9da5f943a2cac375365e6179d83d
Mohammed Alrefai <mohammed@refam.io>
2025-06-04 01:02:19 +0300

    chore(fmt): simplify dprint config
    Restrict dprint to formatting markdown only
    Use nix function to obtain plugin list instead of hardcoded URLs

--- treefmt.nix
+++ treefmt.nix
@@ -1,48 +1,21 @@
-_: {
+{pkgs, ...}: {
   projectRootFile = ".git/config";
   programs = {
     alejandra.enable = true;
     dprint = {
       enable = true;
-      includes = [
-        "**/*.{astro,html,ts,tsx,js,mjs,cjs,jsx,json,jsonc,toml,yaml,yml,md}"
-        "**/*.{graphql,gql,ipynb,py,pyi}"
-        "Dockerfile"
-      ];
-      excludes = ["node_modules" "*-lock.json" "*-lock.yaml" "dist"];
+      includes = ["**/*.md"];
       settings = {
         incremental = true;
         lineWidth = 80;
         indentWidth = 2;
         useTabs = false;
         newLineKind = "lf";
-        typescript = {
-          semiColons = "asi";
-          quoteStyle = "alwaysSingle";
-          "jsx.quoteStyle" = "preferSingle";
-          quoteProps = "asNeeded";
-          useBraces = "preferNone";
-          singleBodyPosition = "sameLine";
-          "arrowFunction.useParentheses" = "preferNone";
-          "typeLiteral.separatorKind" = "comma";
-        };
-        json.trailingCommas = "never";
         markdown.textWrap = "always";
-        markup.quotes = "single";
-        ruff.lineLength = 80;
         # https://plugins.dprint.dev
-        plugins = [
-          "https://plugins.dprint.dev/typescript-0.95.5.wasm"
-          "https://plugins.dprint.dev/json-0.20.0.wasm"
-          "https://plugins.dprint.dev/markdown-0.18.0.wasm"
-          "https://plugins.dprint.dev/toml-0.7.0.wasm"
-          "https://plugins.dprint.dev/dockerfile-0.3.3.wasm"
-          "https://plugins.dprint.dev/ruff-0.3.9.wasm"
-          "https://plugins.dprint.dev/jupyter-0.2.0.wasm"
-          "https://plugins.dprint.dev/g-plane/markup_fmt-v0.20.0.wasm"
-          "https://plugins.dprint.dev/g-plane/pretty_yaml-v0.5.1.wasm"
-          "https://plugins.dprint.dev/g-plane/pretty_graphql-v0.2.1.wasm"
-        ];
+        plugins = pkgs.dprint-plugins.getPluginList (
+          plugins: [plugins.dprint-plugin-markdown]
+        );
       };
     };
     statix.enable = true;
