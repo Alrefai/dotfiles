6e8fc080e6a55380d4e413f229404cfd597fb249
Mohammed Alrefai <mohammed@refam.io>
2026-08-06 17:15:55 +0300

    feat(nixos): use pkg overlay to override plex pkg

--- modules/nixos/hosts/mimacbookpro/plex.nix
+++ modules/nixos/hosts/mimacbookpro/plex.nix
@@ -1,43 +1,47 @@
-{
-  pkgs,
-  username,
-  ...
-}: let
-  # Pinned version of Plex Media Server
-  #
-  # ---
-  # refs:
-  # - https://www.plex.tv/media-server-downloads/?cat=computer&plat=linux
-  version = "1.43.3.10828-00f62d37d";
+/**
+# Pinned version of Plex Media Server
 
-  # Version's SHA-1 Checksum: d73eba8f297785e1b611ed6e1628b2432eaa617e
-  #
-  # Verfiy hash with:
-  # ```
-  # nix-prefetch-url --type sha1 "${url}" "${sha1_hash}"
-  # ```
-  #
-  # Get SHA-256 checksum with:
-  # ```
-  # read -r hash < <(nix-prefetch-url "${url}") &&
-  #   nix-hash --to-sri --type sha256 "${hash}"
-  # ```
-  plexRaw = pkgs.plexRaw.overrideAttrs (_: {
-    inherit version;
-    src = pkgs.fetchurl {
-      url =
-        "https://downloads.plex.tv/plex-media-server-new/${version}/"
-        + "debian/plexmediaserver_${version}_amd64.deb";
-      hash = "sha256-ieU0/7Vlrs2tsR1QhD2Cyk/pia4MfmAugx0Ec6Ook20=";
+Version's SHA-1 Checksum: 02011c3275c67c0c73e170031187972d421b9fc9
+
+Verfiy hash with:
+
+```
+nix-prefetch-url --type sha1 "${url}" "${sha1_hash}"
+```
+
+Get SHA-256 checksum with:
+
+```
+read -r hash < <(nix-prefetch-url "${url}") &&
+  nix-hash --to-sri --type sha256 "${hash}"
+```
+
+---
+refs:
+- https://www.plex.tv/media-server-downloads/?cat=computer&plat=linux
+*/
+{username, ...}: let
+  plexOverlay = final: prev: {
+    plex = prev.plex.override {
+      plexRaw = prev.plexRaw.overrideAttrs rec {
+        version = "1.43.3.10861-07dfddaeb";
+        src = final.fetchurl {
+          url =
+            "https://downloads.plex.tv/plex-media-server-new/${version}/"
+            + "debian/plexmediaserver_${version}_amd64.deb";
+          hash = "sha256-s8OpELTLfdincYQZawp76rsZx5AQXMR6+algH/Ev0zI=";
+        };
+      };
     };
-  });
+  };
 in {
+  nixpkgs.overlays = [plexOverlay];
+
   services = {
     plex = {
       enable = true;
       openFirewall = true;
       dataDir = "/persistent/var/lib/plex";
-      package = pkgs.plex.override {inherit plexRaw;};
     };
     tailscale.serve.services.plex.endpoints = {
       "tcp:443" = "https+insecure://127.0.0.1:32400";
