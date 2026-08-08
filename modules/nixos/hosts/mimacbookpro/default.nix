ddce440385b4afb26235e6d9f8f36c158175aebb
Mohammed Alrefai <mohammed@refam.io>
2026-08-08 21:49:32 +0300

    refactor(nixos): concatenate strings in a list

--- modules/nixos/hosts/mimacbookpro/default.nix
+++ modules/nixos/hosts/mimacbookpro/default.nix
@@ -28,22 +28,26 @@
       ;
   };
 
-  services.tailscale.serve.enable = true;
-  services.tlp.enable = false;
-
-  users.users.${username}.openssh.authorizedKeys = {
-    #! WARNING:
-    #! These public keys are for my machine only.
-    #! If you fork this repo, replace them with your own keys.
-    #! Keys are applied only when the git email matches my identity.
-    keys = lib.mkIf (email == "mohammed" + "@" + "refam.io") [
-      (
-        "ssh-ed25519 "
-        + "AAAAC3NzaC1lZDI1NTE5AAAAIFBh8sQx2EO3OPO85NKlK4IIsF1fFFJ2vpPCpwwuyX6h"
-        + " @1password"
-      )
-    ];
+  services = {
+    tailscale.serve.enable = true;
+    tlp.enable = false;
   };
+
+  /**
+  WARNING:
+  Replace these public keys with your own when you fork this repo.
+  They’re only valid for my machine.
+  Keys are applied only when the git email matches my identity.
+
+  */
+  users.users.${username}.openssh.authorizedKeys.keys = lib
+    .mkIf (email == "mohammed" + "@" + "refam.io") [
+    (builtins.concatStringsSep " " [
+      "ssh-ed25519"
+      "AAAAC3NzaC1lZDI1NTE5AAAAIFBh8sQx2EO3OPO85NKlK4IIsF1fFFJ2vpPCpwwuyX6h"
+      "@1password"
+    ])
+  ];
 
   system.stateVersion = "26.11";
 }
