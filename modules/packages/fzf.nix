6c4e4004f4e1bb7b8a7ad857edafd27d9c6c9075
Mohammed Alrefai <mohammed@refam.io>
2026-08-18 02:18:56 +0300

    feat(flake): add fzf wrapper

---
+++ modules/packages/fzf.nix
@@ -0,0 +1,191 @@
+{
+  pkgs,
+  wrappers,
+  ...
+}: let
+  inherit
+    (pkgs.lib)
+    concatStringsSep
+    filterAttrs
+    mapAttrs
+    mapAttrsToList
+    mkOption
+    mkPackageOption
+    optionals
+    types
+    ;
+
+  module = wrappers.lib.wrapModule ({config, ...}: {
+    /**
+    Options and config were partially taken from home-manager fzf module
+    - github.com/nix-community/home-manager/blob/master/modules/programs/fzf.nix
+    */
+    options = {
+      basePackage = mkPackageOption config.pkgs "fzf" {};
+
+      colors = mkOption {
+        type = types.attrsOf types.str;
+        default = {};
+        example = {
+          bg = "#1e1e1e";
+          "bg+" = "#1e1e1e";
+          fg = "#d4d4d4";
+          "fg+" = "#d4d4d4";
+        };
+        description = ''
+          Color scheme options added to `FZF_DEFAULT_OPTS`. See
+          <https://github.com/junegunn/fzf/wiki/Color-schemes>
+          for documentation.
+        '';
+      };
+
+      defaultCommand = mkOption {
+        type = types.nullOr types.str;
+        default = null;
+        example = "fd --type f";
+        description = ''
+          The command that gets executed as the default source for fzf
+          when running.
+        '';
+      };
+
+      defaultOptions = mkOption {
+        type = types.listOf types.str;
+        default = [];
+        example = [
+          "--height 40%"
+          "--border"
+        ];
+        description = ''
+          Extra command line options given to fzf by default.
+        '';
+      };
+
+      extraEnv = mkOption {
+        type = types.attrsOf types.str;
+        default = {};
+        example = {
+          FZF_DEFAULT_OPTS_FILE = toString (pkgs.writeText "fzf.rc" ''
+            --style=full
+            --border
+            --input-label=' Input '
+            --preview='${pkgs.lib.getExe pkgs.bat} -n --color always -- {}'
+            --preview-window=hidden
+            --bind='ctrl-w:change-preview-window(up|right|)'
+          '');
+        };
+        description = ''
+          Additional environment variables to set for fzf.
+        '';
+      };
+    };
+
+    config = let
+      fzfEnvVars = filterAttrs (_name: value: value != [] && value != null) {
+        FZF_DEFAULT_COMMAND = config.defaultCommand;
+        FZF_DEFAULT_OPTS =
+          config.defaultOptions
+          ++ optionals (config.colors != {}) [
+            "--color '${concatStringsSep "," (
+              mapAttrsToList (name: value: "${name}:${value}") config.colors
+            )}'"
+          ];
+      };
+    in {
+      package = config.basePackage;
+      env = mapAttrs (_: toString) fzfEnvVars // config.extraEnv;
+    };
+  });
+
+  fzf = module.apply {
+    inherit pkgs;
+    /**
+    Override catppuccin-fzf-mocha theme colors on `FZF_DEFAULT_OPTS_FILE`.
+    */
+    colors = {
+      header = "#CDD6F4";
+      label = "#6C7086";
+    };
+    defaultCommand = builtins.concatStringsSep " " [
+      "${pkgs.lib.getExe' pkgs.fd "fd"}"
+      "--type"
+      "f"
+      "--color"
+      "always"
+      "--follow"
+      "--hidden"
+      "--no-require-git"
+      "--exclude"
+      "'Library/'"
+      "--exclude"
+      "'.cache/'"
+      "--exclude"
+      "'.git/'"
+      "2>/dev/null"
+    ];
+    extraEnv = let
+      header = pkgs.writeShellScript "fzf-header" ''
+        awk '{NF=7}1' < <(
+          ls -l --dereference --si --time-style '+%Y-%m-%d %H:%M' \
+            -- "$1" 2>/dev/null
+        )
+      '';
+
+      preview = pkgs.writeShellApplication {
+        name = "fzf-preview";
+        runtimeInputs = [pkgs.bat pkgs.file];
+        text = ''
+          read -r type < <(file --brief --dereference --mime -- "$1")
+
+          if [[ $type =~ =binary || $type =~ image/ ]]; then
+            file --brief --dereference -- "$1"
+            exit
+          fi
+
+          exec bat \
+            --color always --style numbers --pager never --line-range :500 \
+            -- "$1" 2>/dev/null
+        '';
+      };
+
+      listLabel = pkgs.writeShellScript "fzf-transform-list-label" ''
+        if [[ -z $FZF_QUERY ]]; then
+          echo " $FZF_MATCH_COUNT items "
+          exit
+        fi
+
+        echo " $FZF_MATCH_COUNT matches for [$FZF_QUERY] "
+      '';
+
+      previewLabel = pkgs.writeShellScript "fzf-transform-preview-label" ''
+        [[ -n $1 ]] && printf ' Previewing [%s] ' "$1"
+      '';
+    in {
+      FZF_DEFAULT_OPTS_FILE = toString (pkgs.writeText "fzf.rc" ''
+        --style=full
+        --border
+        --input-label=' Input '
+        --header-label=' File Metadata '
+        --preview='${pkgs.lib.getExe preview} {}'
+        --preview-window=hidden
+        --bind='ctrl-w:change-preview-window(up|right|)'
+        --bind=ctrl-v:change-multi
+        --bind=ctrl-a:select-all,ctrl-d:deselect-all,ctrl-t:toggle-all
+        --bind='result:transform-list-label:${listLabel}'
+        --bind='focus:transform-preview-label:${previewLabel} {}'
+        --bind='focus:+transform-header:${header} {}'
+        --no-height
+        --tmux=center,80%,border-native
+        --ansi
+        # Colors were taken from previously generated home-managaer catppuccin
+        # fzf oled mocha theme.
+        --color=bg+:#313244,bg:#000000,spinner:#F5E0DC,hl:#F38BA8
+        --color=fg:#CDD6F4,header:#F38BA8,info:#CBA6F7,pointer:#F5E0DC
+        --color=marker:#B4BEFE,fg+:#CDD6F4,prompt:#CBA6F7,hl+:#F38BA8
+        --color=selected-bg:#45475A
+        --color=border:#6C7086,label:#CDD6F4
+      '');
+    };
+  };
+in
+  fzf.wrapper
