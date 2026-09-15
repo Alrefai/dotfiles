65c2131d03184390ce2aaf8002417e7f4855e59e
Mohammed Alrefai <mohammed@refam.io>
2026-09-15 19:41:53 +0300

    feat(nixos): add server-side encryption for rustfs

--- modules/nixos/hosts/mimacbookpro/rustfs.nix
+++ modules/nixos/hosts/mimacbookpro/rustfs.nix
@@ -1,21 +1,37 @@
 {
   config,
   dataDisksMountpointSuffix,
-  lib,
+  pkgs,
   username,
   ...
 }: let
-  isDataDisk = key: _: lib.hasPrefix dataDisksMountpointSuffix key;
+  inherit
+    (pkgs.lib)
+    concatStringsSep
+    filterAttrs
+    genAttrs
+    getExe
+    hasPrefix
+    length
+    lessThan
+    mapAttrsToList
+    mkForce
+    mkIf
+    pipe
+    sort
+    ;
+
+  isDataDisk = key: _: hasPrefix dataDisksMountpointSuffix key;
 
   mountpoint = _: disk: disk.content.content.mountpoint;
 
-  dataMountpoints = lib.pipe config.disko.devices.disk [
-    (lib.filterAttrs isDataDisk)
-    (lib.mapAttrsToList mountpoint)
-    (builtins.sort builtins.lessThan)
+  dataMountpoints = pipe config.disko.devices.disk [
+    (filterAttrs isDataDisk)
+    (mapAttrsToList mountpoint)
+    (sort lessThan)
   ];
 
-  dataDisksCount = builtins.length dataMountpoints;
+  dataDisksCount = length dataMountpoints;
 
   rule = _: {
     d = {
@@ -25,7 +41,7 @@
     };
   };
 
-  systemdTmpfilesRules = lib.genAttrs dataMountpoints rule;
+  systemdTmpfilesRules = genAttrs dataMountpoints rule;
 in {
   assertions = [
     {
@@ -39,11 +55,16 @@ in {
   services = {
     rustfs = {
       enable = true;
-      environmentFile = lib.mkForce "";
+      environmentFile = mkForce "";
       settings = {
-        RUSTFS_STORAGE_CLASS_RRS = lib.mkIf (dataDisksCount > 1) "EC:1";
-        RUSTFS_STORAGE_CLASS_STANDARD = lib.mkIf (dataDisksCount > 1) "EC:1";
-        RUSTFS_VOLUMES = builtins.concatStringsSep " " dataMountpoints;
+        RUSTFS_STORAGE_CLASS_RRS = mkIf (dataDisksCount > 1) "EC:1";
+        RUSTFS_STORAGE_CLASS_STANDARD = mkIf (dataDisksCount > 1) "EC:1";
+        RUSTFS_VOLUMES = concatStringsSep " " dataMountpoints;
+        RUSTFS_KMS_ENABLE = "true";
+        RUSTFS_KMS_BACKEND = "vault-transit";
+        RUSTFS_KMS_VAULT_MOUNT_PATH = "rustfs";
+        RUSTFS_KMS_VAULT_TRANSIT_METADATA_KV_MOUNT = "rustfs-kv";
+        RUSTFS_KMS_DEFAULT_KEY_ID = "rustfs-default-key";
       };
     };
   };
@@ -71,15 +92,49 @@ in {
         1FV2tVdT58ZLfFMEkVIxnhGiqGM7AMvk8HvMyvro/ZLvntSan1jPTc/+VJCpq041ReCqr \
         OZbKflMxspxnaLRYisg==
     '';
+    rustfs-kms-vault-encrypted-token = ''
+      rustfs-kms-vault-token: \
+        Whxqht+dQJax1aZeCGLxmiAAAAABAAAADAAAABAAAACVtxNmMpiLv+lhoGcAAAAAp5wmF \
+        owVs9XE0qHh+5/5ixucS7RKn/IDHRoB/5mSVE9p4viWhRGJUvHYCdcfGCk3dCqgxejcPS \
+        nEaD1+YJ8aRkn5Zl9c9itxZomFnuzgsVbAJexopUaZrDysEVhqG2XlZShTd1Zxvmh9Jpx \
+        +Td+5Rq0nB1U2AMgn7BgOhw2w4Ai/oiG1t4CrjpXnZxC3151DuJQLxQPoAjTavDH6B2fi \
+        /w==
+    '';
+    kms-vault-encrypted-address = ''
+      kms-vault-address: \
+        Whxqht+dQJax1aZeCGLxmiAAAAABAAAADAAAABAAAACsCLviLIuOmfvrVBIAAAAAukmRw \
+        d9Y5iWB1OjeGce628nyrLN2/vSM9JHr9tYeoOa1O6s/mIB0FEcMgWBVySo6BMCSPpQiMZ \
+        5qaYrLI3+xRYxW7jmw6L2yPyiTBa6nokRowx0IxSKffg==
+    '';
+
+    rustfs-wrapper = pkgs.writeShellScript "rustfs-wrapper" ''
+      TOKEN_FILE='rustfs-kms-vault-token'
+
+      install -m 0400 \
+        "$CREDENTIALS_DIRECTORY/$TOKEN_FILE" \
+        "$RUNTIME_DIRECTORY/$TOKEN_FILE"
+
+      read -r RUSTFS_KMS_VAULT_ADDRESS \
+        <"$CREDENTIALS_DIRECTORY/kms-vault-address"
+
+      export RUSTFS_KMS_VAULT_ADDRESS
+      export RUSTFS_KMS_VAULT_TOKEN_FILE="$RUNTIME_DIRECTORY/$TOKEN_FILE"
+
+      exec ${getExe config.services.rustfs.package}
+    '';
   in {
     services = {
       rustfs = {
-        preStart = lib.mkForce "";
+        preStart = mkForce "";
         serviceConfig = {
+          RuntimeDirectory = "rustfs";
           SetCredentialEncrypted = [
             rustfs-encrypted-access-key
             rustfs-encrypted-secret-key
+            rustfs-kms-vault-encrypted-token
+            kms-vault-encrypted-address
           ];
+          ExecStart = mkForce rustfs-wrapper;
         };
         environment = {
           RUSTFS_ACCESS_KEY_FILE = "%d/rustfs-access-key";
@@ -103,11 +158,11 @@ in {
           Type = "oneshot";
           ExecStart = [
             ''
-              ${lib.getExe config.services.tailscale.package} serve \
+              ${getExe config.services.tailscale.package} serve \
                 --service=svc:rustfs --https=443 9001
             ''
             ''
-              ${lib.getExe config.services.tailscale.package} serve \
+              ${getExe config.services.tailscale.package} serve \
                 --service=svc:s3 --https=443 9000
             ''
           ];
@@ -115,6 +170,6 @@ in {
       };
     };
 
-    tmpfiles.settings."10-rustfs" = lib.mkForce systemdTmpfilesRules;
+    tmpfiles.settings."10-rustfs" = mkForce systemdTmpfilesRules;
   };
 }
